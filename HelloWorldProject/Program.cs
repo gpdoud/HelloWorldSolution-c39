61848c2fae2754f0c8d330501bd6b6fb46f60c0f
Greg Doud <gpdoud@gmail.com>
2023-02-11 06:30:12 -0500

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorldProject;
+
+internal class Program
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    string name = "Greg Doud";
+
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Greg Doud");
+    }
+}
